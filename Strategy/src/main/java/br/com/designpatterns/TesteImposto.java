32c19024fbb7db54b68b644745e7cb95a7dc3986
Thiago Brito Rocha <thiagobritorocha@yahoo.com.br>
2020-09-03 20:48:31 -0300

    Lista de imposto

--- Strategy/src/main/java/br/com/designpatterns/TesteImposto.java
+++ Strategy/src/main/java/br/com/designpatterns/TesteImposto.java
@@ -1,16 +1,25 @@
 package br.com.designpatterns;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TesteImposto {
 
 	public static void main(String[] args) {
+
 		Imposto iss = new ISS();
 		Imposto icms = new ICMS();
 		
+		List<Imposto> lista = new ArrayList<>();
+
+		lista.add(iss);
+		lista.add(icms);
+
 		Orcamento orcamento = new Orcamento(1000.0);
-		
+
 		CalculadorDeImpostos calculadora = new CalculadorDeImpostos();
+
+		lista.forEach(i -> calculadora.realizaCalculo(orcamento, i));
 		
-		calculadora.realizaCalculo(orcamento, iss);
-		calculadora.realizaCalculo(orcamento, icms);
 	}
 }
